b71bdf06854d2628e6395a8b4d820d2baa2678f2
Ben E Camber <bcamber@pbio381.uvm.edu>
2020-02-05 09:25:19 -0500

    made them trimmed fastq files and analyzed them

--- myscripts/trimmedfastqc.sh
+++ myscripts/trimmedfastqc.sh
@@ -4,7 +4,7 @@ cd ~/ecological-genomics/myresults/
 
 mkdir trimmedfastqc
 
-for file in /data/project_data/RS_ExomeSeq/fastq/edge_fastq/pairedcleanreads
+for file in /data/project_data/RS_ExomeSeq/fastq/edge_fastq/pairedcleanreads/MRC*
 
 do
 
