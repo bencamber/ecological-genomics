18375d993fd3e3415f8543a1f092d481550844ce
bencamber <benjamin.camber@uvm.edu>
2020-02-19 11:26:49 -0500

    made SFS and analyzed theta, tajima's d, and made plots

--- myscripts/ANGSD_mypop.sh
+++ myscripts/ANGSD_mypop.sh
@@ -2,4 +2,81 @@ myrepo="/users/b/c/bcamber/ecological-genomics"
 mkdir ${myrepo}/myresults/ANGSD
 output="${myrepo}/myresults/ANGSD"
 mypop="MRC"
-ls /data/project_data/RS_ExomeSeq/mapping/BWA/${mypop}_*sorted.rm*.bam>${output}${mypoop}_bam.list
+ls /data/project_data/RS_ExomeSeq/mapping/BWA/${mypop}_*sorted.rm*.bam >${output}/${mypop}_bam.list
+REF="/data/project_data/RS_ExomeSeq/ReferenceGenomes/Pabies1.0-genome_reduced.fa"
+
+ANGSD -b ${output}/${mypop}_bam.list \
+-ref ${REF} -anc ${REF} \
+-out ${output}/${mypop}_allsites \
+-nThreads 1 \
+-remove_bads 1 \
+-C 50 \
+-baq 1 \
+-minMapQ 20 \
+-minQ 20 \
+-setMinDepth 3 \
+-minInd 2 \
+-setMinDepthInd 1 \
+-setMaxDepthInd 17 \
+-skipTriallelic 1 \
+-GL 1 \
+-doCounts 1 \
+-doMajorMinor 1 \
+-doMaf 1 \
+-doSaf 1 \
+-doHWE 1 \
+# -SNP_pval 1e-6
+
+# Now calculating SFS for the folded minor allele spectra
+ANGSD -b ${output}/${mypop}_bam.list \
+-ref ${REF} -anc ${REF} \
+-out ${output}/${mypop}_allsites \
+-nThreads 1 \
+-remove_bads 1 \
+-C 50 \
+-baq 1 \
+-minMapQ 20 \
+-minQ 20 \
+-setMinDepth 3 \
+-minInd 2 \
+-setMinDepthInd 1 \
+-setMaxDepthInd 17 \
+-skipTriallelic 1 \
+-GL 1 \
+-doCounts 1 \
+-doMajorMinor 1 \
+-doMaf 1 \
+-doSaf 1 \
+-fold 1 
+
+# Get a rough estimate of SFS 
+realSFS ${output}/${mypop}_allsites.saf.idx \
+-maxIter 1000 -tole 1e-6 -P 1 \
+>${output}/${mypop}_outFold.sfs
+
+# get refined estimate of SFS and doTheta
+ANGSD -b ${output}/${mypop}_bam.list \
+-ref ${REF} -anc ${REF} \
+-out ${output}/${mypop}_allsites \
+-nThreads 1 \
+-remove_bads 1 \
+-C 50 \
+-baq 1 \
+-minMapQ 20 \
+-minQ 20 \
+-setMinDepth 3 \
+-minInd 2 \
+-setMinDepthInd 1 \
+-setMaxDepthInd 17 \
+-skipTriallelic 1 \
+-GL 1 \
+-doCounts 1 \
+-doMajorMinor 1 \
+-doMaf 1 \
+-doSaf 1 \
+-fold 1 \
+-pest ${output}/${mypop}_outFold.sfs \
+-doThetas 1
+
+# use the dotheta output from above to estimate nucelotide diversity
+thetaStat do_stat ${output}/${mypop}_allsites.thetas.idx
